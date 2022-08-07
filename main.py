686114e9e01347dee86e3e97d68fd35a3b5554ea
Anthony Le Goff <legoff.ant@gmail.com>
2022-08-07 18:34:53 +0200

    Init Player class

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame, sys 
 
+class Game:
+    """Class principale du jeu"""
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+        # Update all sprite groups
+        # draw all sprite groups
+
 if __name__ == '__main__':
     pygame.init()
     screen_with = 600
     screen_height = 600
     screen = pygame.display.set_mode((screen_with,screen_height))
     clock = pygame.time.Clock()
+    game = Game()
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +25,7 @@ if __name__ == '__main__':
                 sys.exit()
 
         screen.fill((30,30,30))
+        game.run()
 
         pygame.display.flip()
         clock.tick(60)
